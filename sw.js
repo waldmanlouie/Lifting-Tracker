69eed1f17458696c9cbecd97d3be7ef362347113
waldmanlouie <waldman.louis@gmail.com>
2026-03-23 06:00:16 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,47 @@
+var CACHE_NAME = 'lift-v1';
+var URLS_TO_CACHE = [
+  './',
+  './lifting-tracker.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(URLS_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(names) {
+      return Promise.all(
+        names.filter(function(name) { return name !== CACHE_NAME; })
+             .map(function(name) { return caches.delete(name); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(cached) {
+      // Network first, fall back to cache (so updates propagate)
+      return fetch(event.request).then(function(response) {
+        // Update cache with fresh version
+        if (response.ok) {
+          var clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache) {
+            cache.put(event.request, clone);
+          });
+        }
+        return response;
+      }).catch(function() {
+        return cached;
+      });
+    })
+  );
+});
